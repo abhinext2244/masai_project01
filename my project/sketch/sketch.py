106d562ed04614add2f8baa0b471315dce85fb78
abhinext2244 <115467885+abhinext2244@users.noreply.github.com>
2023-07-22 14:45:51 +0530

    Add files via upload

---
+++ my project/sketch/sketch.py
@@ -0,0 +1,7 @@
+from sketchpy import canvas
+obj = canvas.sketch_from_image('C:\\Users\\Ashutosh\\Desktop\\my project\\sketch\\rishav.jpg')
+obj.draw()
+
+# from sketchpy import library as lib
+# sketch= lib.tom_holland()
+# sketch.draw()
